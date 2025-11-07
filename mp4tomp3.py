5e7acfd0e25f64a8cdd2ce86f4b8b4e804752dc6
Darkrider731 <Katzrefael5@gmail.com>
2025-11-07 05:48:47 +0200

    Add MP4 to MP3 conversion and YouTube download functionality

---
+++ mp4tomp3.py
@@ -0,0 +1,55 @@
+import time
+
+from moviepy.editor import *
+import glob
+import os
+from pytube import YouTube
+
+def newest():
+    listfiles = glob.glob('C:\\Users\\Refael\\Downloads\\*.mp4')
+    newestfile = max(listfiles, key = os.path.getctime)
+    return newestfile
+
+def convert():
+    file = newest()
+    filemp3 = file
+    filemp3 = filemp3[:-3]
+    mp3encode = ('mp3')
+    filemp3save = filemp3 + mp3encode
+    video = VideoFileClip(file)
+    video.audio.write_audiofile(filemp3save)
+
+def downloadyt():
+    SAVE_PATH = "C:\\Users\\Refael\\Downloads"
+
+
+
+    link = input("please paste your link")
+    print("please wait while we verify your link")
+    try:
+        yt = YouTube(link)
+    except:
+        print("link invalid")
+
+
+    try:
+        mp4files = yt.streams.filter(progressive=True, file_extension='mp4')
+        print("link accepted download starting")
+    except:
+        print("check your internet connection and try again")
+        time.sleep(1)
+        print("trying again...")
+        time.sleep(1)
+        print("link accepted download starting")
+    d_video = yt.streams.get_highest_resolution()
+
+    try:
+        d_video.download(SAVE_PATH)
+    except:
+        print("error 2")
+
+    print("you're download is now complete and can be found under Downloads in File explorer")
+
+    convert = input("would you like to convert it into an mp3 yes/no")
+    return convert
+
